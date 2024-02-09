16a8209d7241ee9d2866ccaa49afa8fe1e7e8f50
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-09 21:49:02 +0530

    Update main.py
    Added loop

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
 secretNumber = random.randint(1, 20)
 print('I am thinking of a number between 1 and 20.')
+
+# Ask the player to guess 6 times.
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
