bec2d847d343835999406566f9e620dde9215301
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-09 21:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print('I am thinking of a number between 1 and 20.')
 for guessesTaken in range(1, 7):
     print('Take a guess.')
     guess = int(input())
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break    # This condition is the correct guess!
